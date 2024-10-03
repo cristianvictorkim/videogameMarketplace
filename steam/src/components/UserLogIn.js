cbcdbda5b247c5d2c46750820a92f9eaa204d06d
Francisco <franchutocco@gmail.com>
2024-10-02 21:02:34 -0300

    correccion en login

--- steam/src/components/UserLogIn.js
+++ steam/src/components/UserLogIn.js
@@ -5,6 +5,7 @@ const UserLogIn = () => {
       <div className="gradient flex items-center justify-center min-h-screen">
         <form className="p-8 w-full max-w-sm bg-main-color border-2 border-black">
           <h2 className="text-2xl font-semibold mb-6 text-center">Log in</h2>
+          <p className='pb-1'>Username</p>
           <div className="mb-4">
             <input
               type="text"
@@ -12,6 +13,7 @@ const UserLogIn = () => {
               className="w-full p-2 border rounded-lg"
             />
           </div>
+          <p className='pb-1'>Password</p>
           <div className="mb-6">
             <input
               type="password"
