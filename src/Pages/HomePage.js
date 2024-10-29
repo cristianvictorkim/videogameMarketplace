ba80b8548e7d1054d76bf8551dec196b031c62ba
ftorcini <fedetorcini@gmail.com>
2024-10-29 19:28:45 -0300

    Removed Featured

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -7,42 +7,34 @@ import { getGameById } from "../Entities/Game";
 
 const HomePage = () => {    
     const [selection, setSelection] = React.useState([]);
-    const [featured, setFeatured] = React.useState({});
+    const [featured, setFeatured] = React.useState([]);
     
     React.useEffect(() => { 
         fetch("http://localhost:3001/games")
             .then(res => res.json())
             .then(data => setSelection(data));
             
-        getGameById(2).then(data => setFeatured(data));
+        fetch("http://localhost:3001/games")
+            .then(res => res.json())
+            .then(data => setFeatured(data));
     }, [])
 
     return( 
         <div>
-            <Categories/>             
-            <YourSelection/>
-            {
-                selection.map((game, index) => (
-                    <GameCard
-                        key={index}
-                        image={game.bannerUrl}
-                        title={game.title}
-                        price={game.price}
-                        score={game.rating}
-                        gameId={game.gameId} 
-                        publisherId={game.publisherId}
-                    /> 
-                ))
-            }
+            <Categories/>
             <FeaturedSelection/>
             {
+               featured.map((game, index) => (
                 <GameCard
-                    image={featured.bannerUrl}
-                    title={featured.title}
-                    price={featured.price}
-                    score={featured.rating}
-                    gameId={featured.gameId} 
+                    key={index}
+                    image={game.bannerUrl}
+                    title={game.title}
+                    price={game.price}
+                    score={game.rating}
+                    gameId={game.gameId} 
+                    publisherId={game.publisherId}
                 /> 
+            ))
             }
         </div>
     );
