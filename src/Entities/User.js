99cff3279278b7807d4dcd8d208ddc0f405db0ca
ftorcini <fedetorcini@gmail.com>
2024-10-29 19:44:15 -0300

    fixes to user.js

--- src/Entities/User.js
+++ src/Entities/User.js
@@ -1,3 +1,9 @@
+// Array to store multiple game information instances
+import emptyFoto from "../assets/pfp.png";
+
+const today = new Date();
+const userName = '';
+let pfp = emptyFoto;
 let userId = -1;
 
 function getUserId()
@@ -5,38 +11,5 @@ function getUserId()
     return userId;
 }
 
-// Array to store multiple game information instances
-const games = new Map();
-import emptyFoto from "../assets/pfp.png";
-
-const games = new Map();
-const today = new Date();
-const userName = '';
-let pfp = emptyFoto;
-
-// Counter for auto-assigning gameId
-let gameIdCounter = 1;
-
-// Function to add a new game
-function addGame(title, rating, price, genre, os) {
-    const game = {
-        title: title,
-        rating: rating,
-        price: price,
-        gameId: gameIdCounter,
-        purchaseDate: today.toLocaleDateString(),
-        genre: genre,
-        os: os
-    };
-    games.set(gameIdCounter, game);
-    gameIdCounter++;
-}
-
-function getGames() {
-    return Array.from(games.values());
-}
-
-addGame("Sid Meier’s Civilization® VI", 4.7, 59.99, "Strategy", "Windows10/11");
-
 // Exporting the functions and variables for use in other files
-export { addGame, getGames, games, setPfp, getPfp, getUserName, setUserName };
+export { getUserId, /*setPfp, getPfp, getUserName, setUserName*/ };
