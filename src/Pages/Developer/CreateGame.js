69855c2ec865b6df2ae35e1ab0077855264db396
cristianvictorkim <cristianvkim@gmail.com>
2024-11-19 00:11:17 -0300

    nivelar

--- src/Pages/Developer/CreateGame.js
+++ src/Pages/Developer/CreateGame.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import placeholder from 'assets/Misc/placeholder-image.jpg';
 
 const CreateGame = () => {
